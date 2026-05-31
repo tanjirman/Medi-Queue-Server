9699f82ab72c33bf79cc2f2afdb907bf39b073b0
tanjirman <tanjirmahbub07@gmail.com>
2026-05-31 12:13:12 +0600

    Fix the payload

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const verifyToken = async (req, res, next) => {
 
   try {
     const { payload } = await jwtVerify(token, JWKS);
-    //console.log(payload);
+    console.log(payload);
    
     next();
   } catch (error) {
